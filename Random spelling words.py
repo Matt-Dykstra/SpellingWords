f6bbb85cced2663cc547c61c1000e4c3bc5d996d
Matt-Dykstra <mtdykstra@gmail.com>
2020-06-24 17:17:04 -0400

    Update Random spelling words.py

--- Random spelling words.py
+++ Random spelling words.py
@@ -3,32 +3,10 @@
 
 # In[1]:
 
-
 import pandas as pd
 import numpy as np
 
-
 # In[18]:
 
-
 df = pd.read_csv('spelling words.csv')
-df.head()
-
-
-# In[26]:
-
-
 print(df['Words'].sample(n=4, axis=0))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
